81bca6b64b75ac1bfd61b7f8e17d6c40a8412600
Busappagari Shanmukha <shannub556.marolix@gmail.com>
2024-04-09 18:09:47 +0530

    News App

--- News/NewsApp/views.py
+++ News/NewsApp/views.py
@@ -3,18 +3,21 @@ import requests
 # Create your views here.
 def input(request):
     newsapi = 'dbeaad2890e74a0a8370b3ed8f8f09f9'
-    url = f'https://newsapi.org/v2/everything?q=Apple&from=2024-04-07&sortBy=popularity&apiKey={newsapi}'
+    url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}'
     main_page = requests.get(url).json()
     article = main_page['articles']
     head = []
     data=[]
     for ar in article:
-        head.append(ar["description"])
+        x={}
+        x["description"]=ar["description"]
+        x["title"]=ar["title"]
+        x["url"]=ar["url"]
+        x['publishedAt']=ar['publishedAt']
+        x['urlToImage']=ar['urlToImage']
+        head.append(x)
     for i in range(6):
         data.append(head[i])
-        print(f'{i + 1} {head[i]}')
-        print()
     if request.method=="GET":
-
-        return render(request,'base.html',{'data':data})
-    return render(request,'base.html',{'data':data})
+        return render(request,'base.html',{'data':{'data':data,'head':head}})
+    return render(request,'base.html',{'data':{'data':data,'head':head}})
